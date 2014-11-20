5721af9d5dde0cbbc26cc1ebf6bea5adeeca2895
Romi Phadte <romiphadte@gmail.com>
2014-11-20 00:55:31 -0800

    added some update

--- ABP_DBN/particle.py
+++ ABP_DBN/particle.py
@@ -1,6 +1,7 @@
 import itertools
 import util 
 from random import random
+import math 
 
 
 SENSIBLE_SIMULATION=True
@@ -21,8 +22,26 @@ class Particle:
 		elif random_num<=2:
 			self.starting_valve_state = 2
 
-	def update(self):
+		self.observed_dia_bp=0
+		self.observed_mean_bp=0
+		self.observed_sys_bp=0
 
+		self.apparent_dia_bp=0
+		self.apparent_sys_bp=0
+		self.observed_mean_bp=0
+
+		self.starting_valve_state=0
+		random_num = 100*random()
+		if random_num<=1:
+			self.starting_valve_state=1
+		elif random_num<=2:
+			self.starting_valve_state=2
+
+
+		self.ending_valve_state=self.starting_valve_state
+
+
+	def update(self):
 		if not SENSIBLE_SIMULATION:   #TODO WTF why is this happening
 			self.true_pulse_bp= self.true_pulse_bp + 3*random()
 			self.true_mean_bp= self.true_mean_bp + 6*random()
@@ -43,17 +62,89 @@ class Particle:
 
 
 
-		new_event_random=180*random()-1
+		new_event_random_num=180*random()-1
 		
-		new_event_uniform_random=	
+		new_event_random=0
+		if new_event_random_num<0:
+			new_event_random=new_event_random
 
-		if new_event_random<0:
-			new_event_uniform_random=new_event_random
-			
-		elif new_event_random<8:
-			new_event_random
+		elif new_event_random_num<8:
+			new_event_random=new_event_random/8
+
+		self.starting_valve_state=self.ending_valve_state
+
+		if self.starting_valve_state==0:
+			valve_state_continue_time=0
+		else:
+			valve_state_continue_time=min(1,math.pow(math.e, 0.045 - 4.5*random()))
+
+		new_valve_event = 1
+		if valve_state_continue_time==1:
+			new_valve_event=0
+		elif ((math.abs(new_event_random) < valve_state_continue_time) and (new_event_random < 0)):
+			new_valve_event=2
+		elif (math.abs(new_event_random) > valve_state_continue_time):
+			new_valve_event=3
 
 
+		new_event_initial_length=.1*.3*random()	
+		if new_valve_event==0 or new_valve_event==1:
+			new_event_initial_length=math.pow(10,-4)*random()
+
+
+		if new_valve_event==0:
+			self.ending_valve_state=self.starting_valve_state
+		elif new_valve_event==1:
+			self.ending_valve_state=0
+		elif (self.starting_valve_state + new_event_initial_length)>1:
+			self.ending_valve_state+new_valve_event-1
+		else:
+			self.ending_valve_state=0
+
+		if(starting_valve_state==2):
+			x = valve_state_continue_time
+		else:
+			x = 0
+
+		if new_valve_event==3:
+			y=max(0,min(1-new_event_start_offset, new_event_initial_length))
+		else:
+			y=0
+
+		calc = x+y
+
+		bag_time_frac = calc
+		if(calc<0.03):
+			bag_time_frac = 0
+
+		if(self.starting_valve_state==1):
+			x = valve_state_continue_time
+		else:
+			x = 0 
+
+		if new_valve_event==2:
+			y=max(0,min(1-new_event_start_offset, new_event_initial_length))
+		else:
+			y=0
+
+		calc=x+y
+
+		zero_time_frac=calc
+		if calc<.03:
+			zero_time_frac=0
+
+		self.apparent_dia_bp=min(bag_pressure, (1 - bag_time_frac-zero_time_frac)*(self.true_dia_bp+zero_pressure) 
+								+ bag_time_frac * max(bag_pressure + zero_pressure,300) + zero_time_frac*self.zero_pressure)
+
+		self.apparent_mean_pressure=min(bag_pressure, (1 - bag_time_frac-zero_time_frac)*(self.true_mean_bp+zero_pressure) 
+								+ bag_time_frac * max(bag_pressure + zero_pressure,300) + zero_time_frac*self.zero_pressure)
+
+		self.apparent_sys_bp=min(bag_pressure, (1 - bag_time_frac-zero_time_frac)*(self.true_sys_bp+zero_pressure) 
+								+ bag_time_frac * max(bag_pressure + zero_pressure,300) + zero_time_frac*self.zero_pressure)
+
+		self.observed_dia_bp= self.apparent_dia_bp+3*random()
+		self.observed_mean_bp=self.apparent_mean_pressure+random()
+		self.observed_sys_bp=self.apparent_sys_bp+3*random()
 
 
 		# self.true_dia_bp
