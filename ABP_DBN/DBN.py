9b5cb1f3852c1bdaae4142f44533430fb23cdc4b
Sammy Sidhu <sammmy.sidhu@gmail.com>
2014-11-21 17:34:51 -0800

    wrote the reweighting and resampling for observe in the DBN

--- ABP_DBN/DBN.py
+++ ABP_DBN/DBN.py
@@ -1,34 +1,55 @@
 import itertools
 import util 
-from random import random
+import random as rand
 import particle
-
+from util import Counter
+from scipy import stats
 class DBN:
 
-	def __init__(self,samples=6000):
-		self.particles=[particle.Particle() for _ in range(samples)] 
-		self.samples = samples
+    def __init__(self,samples=8000):
+        self.particles=[particle.Particle() for _ in range(samples)] 
+        self.samples = samples
 
-	def elapseTime(self):
-		for particle in particles:
-			particle.update()
+    def elapseTime(self):
+        for particle in particles:
+            particle.update()
 
-		
-		#resample
+        
+        #resample
 
-	def observe(self,observation,sensorModel):
+    def observe(self,observation,sensorModel):
+        return
 
+    def weightAndResample(self,observation):
+        """
+        observation should be taken in a size 3 tuple in the order
+        (dia_bp, mean_bp, sys_bp)
 
+        """
 
-	def resample(self):
-		items = dict()
-		count = 0
-		for item in self.particles:
-			items[count] = item
-			count += 1
-		newList = []
-		for i in range(self.samples):
-			newList.append(items[int(random()*self.samples)])
-		self.particles = newList
+        items = dict()
+        dia_bp = observation[0]
+        mean_bp = observation[1]
+        sys_bp = observation[2]
 
-	def getDist(self):
+        particles_dia_bp = []
+        particles_mean_bp = []
+        particles_sys_bp = []
+
+        for part in self.particles:
+            particles_dia_bp.append(part.observed_dia_bp)
+            particles_mean_bp.append(part.observed_mean_bp)
+            particles_sys_bp.append(part.observed_sys_bp)
+
+        w1 = stats.norm.pdf(dia_bp.dia_bp,particles_dia_bp,3)
+        w2 = stats.norm.pdf(mean_bp,particles_mean_bp,1)
+        w3 = stats.norm.pdf(sys_bp,particles_sys_bp,3)
+
+        totalWeight = w1*w2*w3
+
+        for i,part in enumerate(self.particles):
+            items[i] = part
+
+        samp = nSample(totalWeight,list(range(self.samples)))
+
+        self.samples = [items[s] for s in samp]
