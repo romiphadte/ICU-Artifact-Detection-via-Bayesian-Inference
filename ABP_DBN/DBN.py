93fe15fca3386f48804e325f4258ee8d0ab4b3f4
Sammy Sidhu <sammmy.sidhu@gmail.com>
2014-11-22 20:16:13 -0800

    got the particle filter to work but the data isn't really right. The particles all converge after a time step

--- ABP_DBN/DBN.py
+++ ABP_DBN/DBN.py
@@ -2,23 +2,26 @@ import itertools
 import util 
 import random as rand
 import particle
-from util import Counter
+from util import Counter,nSample    
 from scipy import stats
+import matplotlib.pyplot as plt
 class DBN:
 
     def __init__(self,samples=8000):
         self.particles=[particle.Particle() for _ in range(samples)] 
         self.samples = samples
+        self.elapseTime()
+
 
     def elapseTime(self):
-        for particle in particles:
+        for particle in self.particles:
             particle.update()
 
         
         #resample
 
-    def observe(self,observation,sensorModel):
-        return
+    def observe(self,observation):
+        self.weightAndResample(observation)
 
     def weightAndResample(self,observation):
         """
@@ -41,15 +44,37 @@ class DBN:
             particles_mean_bp.append(part.observed_mean_bp)
             particles_sys_bp.append(part.observed_sys_bp)
 
-        w1 = stats.norm.pdf(dia_bp.dia_bp,particles_dia_bp,3)
+        w1 = stats.norm.pdf(dia_bp,particles_dia_bp,3)
         w2 = stats.norm.pdf(mean_bp,particles_mean_bp,1)
         w3 = stats.norm.pdf(sys_bp,particles_sys_bp,3)
 
         totalWeight = w1*w2*w3
-
         for i,part in enumerate(self.particles):
             items[i] = part
+        samp = nSample(totalWeight,list(range(self.samples)),self.samples)
 
-        samp = nSample(totalWeight,list(range(self.samples)))
+        self.particles = [items[s] for s in samp]
 
-        self.samples = [items[s] for s in samp]
+    def getStats(self):
+        stats_dia_bp = [] 
+        stats_mean_bp = []
+        stats_sys_bp = []
+        stats_bag_pr = []
+
+        for part in self.particles:
+            stats_dia_bp.append(part.observed_dia_bp)
+            stats_mean_bp.append(part.observed_mean_bp)
+            stats_sys_bp.append(part.observed_sys_bp)
+            stats_bag_pr.append(part.bag_pressure)
+
+        toRtn = {"dia_bp" : (stats.nanmean(stats_dia_bp), stats.nanstd(stats_dia_bp)),
+                "mean_bp" : (stats.nanmean(stats_mean_bp), stats.nanstd(stats_mean_bp)),
+                "sys_bp" : (stats.nanmean(stats_sys_bp), stats.nanstd(stats_sys_bp)),
+                "bag_pressure" : (stats.nanmean(stats_bag_pr), stats.nanstd(stats_bag_pr)),
+                }
+        # plt.hist(stats_dia_bp)
+        # plt.show()
+        return toRtn
+
+
+        
--- ABP_DBN/run.py
+++ ABP_DBN/run.py
@@ -1,12 +1,25 @@
 import csv
+import DBN
 
-
-f= file("dataOutSimulator.csv")
-lines = f.readlines()
-end = lines.index('\n')
-obs = lines[1:end]
-data = map(lambda x: tuple(map(float,x.split(','))),obs)
-print data
+def getData(inp="dataOutSimulator.csv"):
+	f = file(inp)
+	lines = f.readlines()
+	end = lines.index('\n')
+	obs = lines[1:end]
+	data = map(lambda x: tuple(map(float,x.split(','))),obs)
+	return data
 
 	
-
+def main():
+	data = getData()
+	bayesNet = DBN.DBN()
+	dataOut = []
+	count = 0
+	dataPoints = len(data)
+	for each in data:
+		print("timestep: " + str(count) +  " Observation: " + str(each))
+		bayesNet.elapseTime()
+		bayesNet.observe(each)
+		print bayesNet.getStats()
+		count += 1
+	return dataOut
