68475502cc36099302e6dfccfbe9ca5ac892ac01
Sammy Sidhu <sammmy.sidhu@gmail.com>
2014-11-23 00:06:14 -0800

    added a fix for when totalweights is 0. It completely runs but the plot looks off?

--- ABP_DBN/run.py
+++ ABP_DBN/run.py
@@ -1,12 +1,13 @@
 import csv
 import DBN
 
-def getData(inp="dataOutSimulator.csv"):
+def getData(inp="dataset7.txt"):
 	f = file(inp)
 	lines = f.readlines()
-	end = lines.index('\n')
-	obs = lines[1:end]
-	data = map(lambda x: tuple(map(float,x.split(','))),obs)
+	data = (map(float,l.split(" ")[:3]) for l in lines)
+	# end = lines.index('\n')
+	# obs = lines[1:end]
+	# data = map(lambda x: tuple(map(float,x.split(','))),obs)
 	return data
 
 	
@@ -15,11 +16,11 @@ def main():
 	bayesNet = DBN.DBN()
 	dataOut = []
 	count = 0
-	dataPoints = len(data)
 	for each in data:
 		print("timestep: " + str(count) +  " Observation: " + str(each))
-		bayesNet.elapseTime()
 		bayesNet.observe(each)
-		print bayesNet.getStats()
+		bayesNet.elapseTime()
+		dataOut.append(bayesNet.getStats())
 		count += 1
 	return dataOut
+
