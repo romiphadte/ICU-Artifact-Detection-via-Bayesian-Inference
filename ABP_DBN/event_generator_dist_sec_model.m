fd9b12518184120ba518b9da57a11d044511165b
Romi Phadte <romiphadte@gmail.com>
2015-01-08 09:49:35 -0800

    this has the two different taus (that we can play around with) in it and the averaged minutes -claire

--- ABP_DBN/event_generator_dist_sec_model.m
+++ ABP_DBN/event_generator_dist_sec_model.m
@@ -1,34 +1,101 @@
 clear
 close all
 clc
+
 % a simple model to explain bag-pressure artifact in 1 second resolution
 %%
-T=60000;
-s=zeros(T,1);
-x=zeros(T,1);
-d =zeros(T,1);
+sensibleSimulation = 1;
+T = 60000;
+s = zeros(T,1);
+x = zeros(T,1);
+d = zeros(T,1);
 p = zeros(T,1);
 z = zeros(T,1);
 dz = zeros(T,1);
-apparent=zeros(T,1);
-baselevel=100;
-bagpressure=230;
+truePulseBP = zeros(T,1);
+trueMeanBP = zeros(T,1);
+bagPressure = zeros(T,1);
+zeroPressure = zeros(T,1);
+trueSystolicFraction = zeros(T,1);
+trueDiaBP = zeros(T,1);
+trueSysBP = zeros(T,1);
+apparentMeanBP = zeros(T,1);
+apparentSysBP = zeros(T,1);
+apparentDiaBP = zeros(T,1);
+
+baselevel = 100;
+bagpressure = 230;
 zeropressure = 0;
-tau=5;      % time constant for apparent pressure, apparent does not jump to
+
+
+tau1 = 30;   % time constant for apparent pressure, apparent does not jump to
             % bag-pressure immediately, but climbs in a smooth 1st order
-            % fashion. (think about an RC circuit)
+            % fashion. 
+tau2 = 5;  % This is the RC constant for returning to the regular state
+
 % Initialize
 t=1;
-s(t)=0;
+prevState = 0;
+truePulseBP(t) = 50 + 10*randn();
+trueMeanBP(t) = 95 + 15*randn();    % eq 2
+bagPressure(t) = 230 + 40*randn(); 
+zeroPressure(t) = 0;    % eq 6
+trueSystolicFraction(t) =  0.33 + 0.04*randn();     % eq 3
+trueDiaBP(t) =  trueMeanBP(t) - truePulseBP(t)*trueSystolicFraction(t);     % eq 4
+trueSysBP(t) =  trueMeanBP(t) + truePulseBP(t)*(1-trueSystolicFraction(t)); % eq 5
+
+%time spent in state 1: .005 state -1: 5.7e-4 
+randState = 1000*rand();
+
+if (randState < 6)
+    s(t) = 1;
+    apparentMeanBP(t) = bagPressure(t);
+    apparentSysBP(t)= bagPressure(t);
+    apparentDiaBP(t) = bagPressure(t);
+elseif (randState == 6)
+    s(t)=-1;
+    apparentMeanBP(t)=zeroPressure(t);
+    apparentSysBP(t)= zeroPressure(t);
+    apparentDiaBP(t) = zeroPressure(t);
+
+else
+    s(t)=0; 
+    apparentMeanBP(t)= trueMeanBP(t);
+    apparentSysBP(t)= trueSysBP(t);
+    apparentDiaBP(t)= trueDiaBP(t);
+
+end
+    
 x(t)=0;
-apparent(t)=baselevel;
+
 %%
 for t=2:T;
+    truePulseBP(t) = truePulseBP(t-1) + (3/60)*randn(); %% were these originally 3 and 6?
+    trueMeanBP(t) = trueMeanBP(t-1) + (6/60)*randn();
+    zeroPressure(t) = zeroPressure(t-1);
+    trueSystolicFraction(t) = trueSystolicFraction(t-1) + (0.01/60)*randn(); 
+    
+    if(sensibleSimulation)
+        truePulseBP(t) = truePulseBP(t-1);
+        trueMeanBP(t) = trueMeanBP(t-1);
+        trueSystolicFraction(t) = trueSystolicFraction(t-1);
+    end
+    trueDiaBP(t) =  trueMeanBP(t) - truePulseBP(t)*trueSystolicFraction(t);     % eq 4
+    trueSysBP(t) =  trueMeanBP(t) + truePulseBP(t)*(1-trueSystolicFraction(t)); 
+    r=randi(200*60,1,1);
+
+    if(r==1)    % eq 7
+        bagPressure(t) = 250 + 30*randn();  
+    else
+        bagPressure(t) = (1-(0.001)/60)*bagPressure(t-1);
+    end
+
     if(s(t-1)==0)
-         if(-5e7*(log(rand())) - 20000  > x(t-1))
+        if(-5e7*(log(rand())) - 20000  > x(t-1))
             s(t)=s(t-1);
             x(t)=x(t-1)+1;
-         else
+        else
+            prevState = 0;
             if (rand()*9 < 1)
                 z(t)=1;
                 s(t)=-1;
@@ -44,6 +111,7 @@ for t=2:T;
             s(t)=s(t-1);
             x(t)=x(t-1)+1;
         else
+            prevState = 1;
             s(t)=0;
             p(t) = x(t-1)+1;
             x(t)=0;
@@ -53,6 +121,7 @@ for t=2:T;
             s(t)=s(t-1);
             x(t)=x(t-1)+1;
         else
+            prevState = -1;
             s(t)=0;
             dz(t) = x(t-1)+1;
             x(t)=0;
@@ -60,16 +129,76 @@ for t=2:T;
     end
     
     if (s(t) == 0)
-        newPot = baselevel;
+        newPotM = trueMeanBP(t);
+        newPotS = trueSysBP(t);
+        newPotD = trueDiaBP(t);
     elseif (s(t) == 1)
-        newPot = bagpressure;
+        newPotM = bagPressure(t);
+        newPotS = bagPressure(t);
+        newPotD = bagPressure(t);
     else
-        newPot = zeropressure;
+        newPotM = zeroPressure(t);
+        newPotS = zeroPressure(t);
+        newPotD = zeroPressure(t);
     end
-%     apparent(t)=1/(tau+1)*(tau*apparent(t-1)+ (1-s(t))*baselevel+s(t)*bagpressure );
-    apparent(t)=1/(tau+1)*(tau*apparent(t-1)+ newPot );
+    
+    if (prevState == 0)
+        tau = tau1;
+    else
+        tau = tau2;
+    end  
+    apparentMeanBP(t)=1/(tau+1)*(tau*apparentMeanBP(t-1)+ newPotM );
+    apparentSysBP(t)=1/(tau+1)*(tau*apparentSysBP(t-1)+ newPotS );
+    apparentDiaBP(t)=1/(tau+1)*(tau*apparentDiaBP(t-1)+ newPotD );
+        
+    
 end
 %%
+minutes = T/60;
+minuteMeanBP = zeros(minutes, 1);
+minuteDiaBP = zeros(minutes, 1);
+minuteSysBP = zeros(minutes, 1);
+i = 1;
+
+for t=0:(minutes-1);
+    first = (t*60)+1;
+    last = first+59;
+    minuteMeanBP(i) = sum(apparentMeanBP(first:last))/60;
+    minuteDiaBP(i) = sum(apparentDiaBP(first:last))/60;
+    minuteSysBP(i) = sum(apparentSysBP(first:last))/60;
+    i = i + 1;
+end
+
+secondMeanBP = zeros(T,1);
+secondDiaBP = zeros(T,1);
+secondSysBP = zeros(T,1);
+
+for t=1:T;
+    if (t < 60)
+        val1 = minuteMeanBP(1);
+        val2 = minuteDiaBP(1);
+        val3 = minuteSysBP(1);
+    elseif (mod(t, 60) == 0)
+        val1 = minuteMeanBP(t/60);
+        val2 = minuteDiaBP(t/60);
+        val3 = minuteSysBP(t/60);
+    else
+        prev = minuteMeanBP(floor(t/60));
+        next = minuteMeanBP(floor(t/60)+1);
+        val1 = prev + (((next-prev)/60) * mod(t,60));
+        prev = minuteDiaBP(floor(t/60));
+        next = minuteDiaBP(floor(t/60)+1);
+        val2 = prev + (((next-prev)/60) * mod(t,60)); 
+        prev = minuteSysBP(floor(t/60));
+        next = minuteSysBP(floor(t/60)+1);
+        val3 = prev + (((next-prev)/60) * mod(t,60)); 
+    end
+    secondMeanBP(t) = val1;
+    secondDiaBP(t) = val2;
+    secondSysBP(t) = val3;
+end
+
+
 h=figure; h_zoom = zoom(h);
 set(h_zoom,'Motion','horizontal','Enable','on');
 set(h,'Position',[100 100 1000 500])
@@ -77,10 +206,15 @@ h1=subplot(1,2,1);
 plot(1:T,s,'LineWidth',2)
 h2=subplot(1,2,2);
 hold on
-plot(1:T,baselevel*ones(1,T),'g','LineWidth',1)
-plot(1:T,bagpressure*ones(1,T),'r','LineWidth',1)
-plot(1:T,apparent,'LineWidth',2)
+plot(1:T,bagPressure,'r','LineWidth',1)
+plot(1:T,apparentMeanBP,'LineWidth',2)
+plot(1:T,apparentSysBP,'LineWidth',2)
+plot(1:T,apparentDiaBP,'LineWidth',2)
+plot(1:T,secondMeanBP, 'y', 'LineWidth', 3)
+plot(1:T,secondSysBP,'y', 'LineWidth', 3)
+plot(1:T,secondDiaBP,'y', 'LineWidth', 3)
+
 hold off
-legend('base level','bag pressure','apparent')
+legend('bag pressure','apparentMeanBP','apparentSysBP','apparentDiaBP', 'secondMeanBP')
 ylim([0 300])
 linkaxes([h1,h2],'x');
