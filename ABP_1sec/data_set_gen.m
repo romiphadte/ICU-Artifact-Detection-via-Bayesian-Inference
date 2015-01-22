335bd87ccb9e0bbae3636b6c13a0c5d53ac5aec7
Claire Asselstine <claire.asselstine@berkeley.edu>
2015-01-21 23:03:13 -0800

    data set generator

---
+++ ABP_1sec/data_set_gen.m
@@ -0,0 +1,24 @@
+clear 
+close all
+clc
+%%
+T=1000;
+x = zeros(14,T);
+x(:,1) = abp_prior();
+for t=2:T;
+    x(:,t) = abp_prob(x(:,t-1));
+end
+obs_dia = x(8,:) + 0.5*randn(1,T);
+obs_mean = x(9,:) + 0.5*randn(1,T);
+obs_sys = x(10,:) + 0.5*randn(1,T);
+%%
+figure;
+plot(x(8:10,:)');
+hold on;
+plot(obs_dia);
+plot(obs_mean);
+plot(obs_sys);
+hold off;
+legend('dia','mean','sys')
+out = dataset(obs_mean.',obs_sys.',obs_dia.',x(2,:).',x(5,:).',x(4,:).',x(7,:).');
+export(out);
