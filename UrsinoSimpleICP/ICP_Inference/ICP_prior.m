ab3ca7d210ce6f1ed3924cfc8de2a7a3ac8654d2
Claire Asselstine <claire.asselstine@gmail.com>
2015-04-09 00:40:48 -0700

    prior update

--- UrsinoSimpleICP/ICP_Inference/ICP_prior.m
+++ UrsinoSimpleICP/ICP_Inference/ICP_prior.m
@@ -1,12 +1,13 @@
-function [Pic,Pc,Ca,Va,q] = ICPsimulator(delT,Pa,Pvs,I,Ro,kE,G,tau)
-T = length(Pa);
+function x_prior = ICP_prior()
+%function [Pic,Pc,Ca,Va,q] = ICPsimulator(delT,Pa,Pvs,I,Ro,kE,G,tau)
+x_prior=zeros(7,1);
 % Allocation
-Pic = zeros(1,T);       % Intracranial Pressure
-Pc  = zeros(1,T);       % Capillary Pressure
-Ca  = zeros(1,T);       % Arterial Compliance
-Va  = zeros(1,T);       % Arteriolar Blood Volume
-q   = zeros(1,T);       % Cerebral Blood Flow
-sigma_Gx = zeros(1,T);  % Autoregulation Equilibrium Compliance
+Pic = 0;       % Intracranial Pressure
+Pc = 0;       % Capillary Pressure
+Ca = 0;       % Arterial Compliance
+Va  = 0;       % Arteriolar Blood Volume
+q   = 0;       % Cerebral Blood Flow
+sigma_Gx = 0;  % Autoregulation Equilibrium Compliance
 
 % Parameters that won't be learned
 Rpv = 1.24;
@@ -18,14 +19,22 @@ kR = 4.91*1e4;
 qn = 12.5;
 
 % initialization
-t = 1;
-Ca(t) = Can;    % for DBN, should be initialized according to a prior
-Pic(t) = 9.5;   % for DBN, should be initialized according to a prior
-Va(t) = Ca(t)*(Pa(t) - Pic(t));
-Ra = kR*Can^2/Va(t)^2;
-Pc(t) = (Pa(t)*Rpv + Pic(t)*Ra)/(Rpv+Ra);
-q(t) = (Pa(t)-Pc(t))/Ra;
-x = (q(t) - qn)/qn;
+Ca = Can;    % for DBN, should be initialized according to a prior
+Pic = 10 + 3*randn();   % for DBN, should be initialized according to a prior
+Va = Ca*(Pa - Pic);
+Ra = kR*Can^2/Va^2;
+Pc = (Pa*Rpv + Pic*Ra)/(Rpv+Ra);
+q = (Pa-Pc)/Ra;
+x = (q - qn)/qn;
 delCa = (x<=0)*delCa1 + (x>0)*delCa2;
 k_sigma = delCa/4;
-sigma_Gx(t) = ( (Can+delCa/2)+(Can-delCa/2)*exp(G*x/k_sigma) )/(1+exp(G*x/k_sigma));
+sigma_Gx = ( (Can+delCa/2)+(Can-delCa/2)*exp(G*x/k_sigma) )/(1+exp(G*x/k_sigma));
+I = 0;
+
+x_prior(1)=Pic; 
+x_prior(2)=Pc; 
+x_prior(3)=Ca;
+x_prior(4)=Va; 
+x_prior(5)=q; 
+x_prior(6)=sigma_Gx;
+x_prior(7)=I;
