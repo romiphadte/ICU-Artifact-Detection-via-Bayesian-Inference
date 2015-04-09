99cd36c575a453704d52903a6b486ea2f7a2b918
Romi Phadte <romiphadte@gmail.com>
2015-04-09 01:27:34 -0700

    changed 'next' and 'curr' added random noise. Also added vectorized logic to handle I

--- UrsinoSimpleICP/ICP_Inference/ICP_prob.m
+++ UrsinoSimpleICP/ICP_Inference/ICP_prob.m
@@ -1,35 +1,41 @@
-function x_next = ICP_prob(x_curr)
+function x_next = ICP_prob(x_curr,Pa_next,Pvs_next,Pa_curr,Pvs_curr,Ro,kE,G,tau)
 particles = size(x_curr,2);
 x_next = zeros(7,particles);
 
-Pic= x_curr(1,:); 
-Pc = x_curr(2,:); 
-Ca = x_curr(3,:);
-Va = x_curr(4,:); 
-q = x_curr(5,:); 
-sigma_Gx = x_curr(6,:);
-I = x_curr(7,:); 
+I_same_state = 0.9999
+I_diff_State = 0.0001 
+drain_constant=0.66
 
-    Ca(t) = Ca(t-1) + delT/tau*(-Ca(t-1)+sigma_Gx(t-1)); % for DBN, there should be some additive noise
-    Pic(t) = delT*kE*Pic(t-1)/(1+Ca(t-1)*kE*Pic(t-1))*...
-        (   Ca(t-1)*(Pa(t)-Pa(t-1))/delT + ...
-        (Ca(t)-Ca(t-1))/delT*(Pa(t-1)-Pic(t-1)) + ...
-        (Pc(t-1)-Pic(t-1))/Rf - ...
-        (Pic(t-1)-Pvs(t-1))/Ro + I(t-1) ) + Pic(t-1); % for DBN, there should be some additive noise
-    
-    Va(t) = Ca(t)*(Pa(t) - Pic(t));
-    Ra = (kR*Can^2)/(Va(t)^2);
-    Pc(t) = (Pa(t)*Rpv + Pic(t)*Ra)/(Rpv+Ra);
-    q(t) = (Pa(t)-Pc(t))/Ra;
-    x = (q(t) - qn)/qn;
-    delCa = (x<=0)*delCa1 + (x>0)*delCa2;
-    k_sigma = delCa/4;
-    sigma_Gx(t) = ( (Can+delCa/2)+(Can-delCa/2)*exp(G*x/k_sigma) )/(1+exp(G*x/k_sigma));  
+Pic_curr= x_curr(1,:); 
+Pc_curr = x_curr(2,:); 
+Ca_curr = x_curr(3,:);
+Va_curr = x_curr(4,:); 
+q_curr = x_curr(5,:); 
+sigma_Gx_curr = x_curr(6,:);
+I_curr = x_curr(7,:); 
 
-x_next(1,:)=Pic;
-x_next(2,:)=Pc;
-x_next(3,:)=Ca;
-x_next(4,:)=Va;
-x_next(5,:)=q;
-x_next(6,:)=sigma_Gx;
-x_next(7,:)=I;
+I_next=(I_curr==(randn<=I_same_state))
+
+Ca_next = Ca_curr + delT/tau*(-Ca_curr+sigma_Gx_curr) + 1.0*randn(); % for DBN, there should be some additive noise
+Pic_next = delT*kE*Pic_curr/(1+Ca_curr*kE*Pic_curr)*...
+    (   Ca_curr*(Pa_next-Pa_curr)/delT + ...
+    (Ca_next-Ca_curr)/delT*(Pa_curr-Pic_curr) + ...
+    (Pc_curr-Pic_curr)/Rf - ...
+    (Pic_curr-Pvs_curr)/Ro + I_curr*drain_constant) + Pic_curr + 1.0*randn() ; % for DBN, there should be some additive noise
+
+Va_next = Ca_next*(Pa_next - Pic_next);
+Ra = (kR*Can^2)/(Va_next^2);
+Pc_next = (Pa_next*Rpv + Pic_next*Ra)/(Rpv+Ra);
+q_next = (Pa_next-Pc_next)/Ra;
+x = (q_next - qn)/qn;
+delCa = (x<=0)*delCa1 + (x>0)*delCa2;
+k_sigma = delCa/4;
+sigma_Gx_next = ( (Can+delCa/2)+(Can-delCa/2)*exp(G*x/k_sigma) )/(1+exp(G*x/k_sigma));  
+
+x_next(1,:)=Pic_next;
+x_next(2,:)=Pc_next;
+x_next(3,:)=Ca_next;
+x_next(4,:)=Va_next;
+x_next(5,:)=q_next;
+x_next(6,:)=sigma_Gx_next;
+x_next(7,:)=I_next;
